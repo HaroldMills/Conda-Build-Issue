8d1ac1feddc46b381132bc740f161539804957c6
Harold Mills <harold.mills@gmail.com>
2018-08-22 15:22:33 -0400

    Added various files.

---
+++ conda_build_issue/hello.py
@@ -0,0 +1,9 @@
+import keras
+
+
+def main():
+    print('Hello, {}!'.format(keras.__name__))
+    
+    
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='conda_build_issue',
+    version='0.0.0',
+    description='Conda build issue',
+    author='Harold Mills',
+    author_email='harold.mills@gmail.com',
+    packages=find_packages(),
+    install_requires=['keras'],
+    entry_points={'console_scripts': ['hello=conda_build_issue.hello:main']},
+    zip_safe=False
+)
